21e9c4dcbbedce382bae605e226ccc1f8c0bbcff
g1527066 <g1527066@st.t-kougei.ac.jp>
2017-01-10 16:17:52 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  ///DB1に接続
+  var Connection=require('tedious').Connection;
+  var config={
+    userName: 'mikado',
+    password:'P@ssword',
+    server:'',
+    options:{}
+  };
+  
+  var connection=new Connection(config);
+  connection.on('connect',function(err){
+    if(err){
+      res.render('index',{title:"はじめての DB",message:err});
+    }else{
+      console.log("Connected");
+      executeStatement();
+    }
+  });
+  
+  
   res.render('index', { title: 'Express' });
 });
 
